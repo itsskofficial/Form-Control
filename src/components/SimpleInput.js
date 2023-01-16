23fa0d3a4ed8f35fc98ceaa9562018c1ae5b8fcc
SK <sarthakkarandikar03@gmail.com>
2023-01-16 00:27:21 +0000

    Jan 16, 2023, 5:57 AM

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -30,7 +30,7 @@ const SimpleInput = (props) => {
         <label htmlFor='name'>Your Name</label>
         <input value={enteredName} ref={nameInputRef} type='text' id='name' onChange={inputChangeHandler}/>
       </div>
-      {enteredNameValidity && <p></p>}
+      {enteredNameValidity && <p>Name cannot be empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
